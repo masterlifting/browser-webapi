bad04b0196342b6e0fa125155a34b4859ef56322
Andrei Pestunov <masterlifting@gmail.com>
2025-06-23 22:22:47 +0200

    Refactor error message in try_find_tab function for clarity. Remove redundant text from not found error message.

--- src/browser/page/mod.rs
+++ src/browser/page/mod.rs
@@ -22,7 +22,7 @@ fn try_find_tab(tab_id: &str) -> Result<Arc<Tab>, Error> {
     .unwrap()
     .get(tab_id)
     .cloned()
-    .ok_or_else(|| Error::NotFound(format!("tab_id {} not found", tab_id)))
+    .ok_or_else(|| Error::NotFound(format!("tab_id {}", tab_id)))
 }
 
 pub async fn load(req: web::Json<LoadRequest>, browser: Arc<Browser>) -> HttpResponse {
