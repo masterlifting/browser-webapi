b1c482c3ec029d5a0475aa376d2ae5063ce7f1f4
Andrei Pestunov <masterlifting@gmail.com>
2025-09-01 00:47:05 +0200

    Refactor Docker configuration and update API launch function: adjust Dockerfile context, streamline environment variables, and modify launch function to accept user data directory as a parameter.

--- src/browser/api.rs
+++ src/browser/api.rs
@@ -4,14 +4,14 @@ use std::io::{Error, ErrorKind};
 use std::sync::Arc;
 use std::time;
 
-pub fn launch() -> Result<Arc<Browser>, Error> {
+pub fn launch(user_data_dir: &str) -> Result<Arc<Browser>, Error> {
   let one_week = time::Duration::from_secs(60 * 60 * 24 * 7);
 
   LaunchOptionsBuilder::default()
     .headless(false)
     .disable_default_args(true)
     .ignore_certificate_errors(false)
-    .window_size(Some((1200, 800)))
+    .window_size(Some((1920, 1080)))
     .idle_browser_timeout(one_week)
     .args(vec![
       OsStr::new("--no-sandbox"),
@@ -28,7 +28,7 @@ pub fn launch() -> Result<Arc<Browser>, Error> {
       OsStr::new("--disable-web-security"),
       OsStr::new("--disable-extensions"),
       OsStr::new("--no-default-browser-check"),
-      OsStr::new("--user-data-dir=D:\\private\\browser-webapi\\tmp"),
+      OsStr::new(&format!("--user-data-dir={}", user_data_dir)),
       OsStr::new("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"),
     ])
     .build()
--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,14 @@ async fn main() -> std::io::Result<()> {
   dotenv::dotenv().ok();
 
   let log_level = env::var("LOG_LEVEL").unwrap_or_else(|_| "info".to_string());
+  let user_data_dir = env::var("USER_DATA_DIR").expect("USER_DATA_DIR must be set");
 
   tracing_subscriber::registry()
     .with(tracing_subscriber::EnvFilter::new(log_level))
     .with(tracing_subscriber::fmt::layer())
     .init();
 
-  browser::api::launch().map(web_api::server::run)?.await
+  browser::api::launch(&user_data_dir)
+    .map(web_api::server::run)?
+    .await
 }
