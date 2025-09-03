24d0919b115810160c654d3e7dfa630454d2d6b0
Andrei Pestunov <masterlifting@gmail.com>
2025-09-03 02:28:41 +0200

    Refactor Docker configuration and update browser API launch options: change base image to slim version, optimize build steps, and enhance launch function to accept user data directory.

--- src/browser/api.rs
+++ src/browser/api.rs
@@ -8,28 +8,35 @@ use crate::browser::models::LaunchOptions;
 pub fn launch(options: LaunchOptions) -> Result<Arc<Browser>, Error> {
   LaunchOptionsBuilder::default()
     .headless(options.headless)
+    .path(Some(std::path::PathBuf::from(options.user_data_dir)))
     .disable_default_args(true)
     .ignore_certificate_errors(false)
     .window_size(Some((1920, 1080)))
     .idle_browser_timeout(options.idle_timeout)
-    .args(vec![
-      OsStr::new("--no-sandbox"),
-      OsStr::new("--disable-setuid-sandbox"),
-      OsStr::new("--disable-dev-shm-usage"),
-      OsStr::new("--disable-accelerated-2d-canvas"),
-      OsStr::new("--no-first-run"),
-      OsStr::new("--no-zygote"),
-      OsStr::new("--disable-gpu"),
-      OsStr::new("--hide-scrollbars"),
-      OsStr::new("--mute-audio"),
-      OsStr::new("--disable-infobars"),
-      OsStr::new("--disable-breakpad"),
-      OsStr::new("--disable-web-security"),
-      OsStr::new("--disable-extensions"),
-      OsStr::new("--no-default-browser-check"),
-      OsStr::new(&format!("--user-data-dir={}", options.user_data_dir)),
-      OsStr::new("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"),
-    ])
+    .args(
+      [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-accelerated-2d-canvas",
+        "--no-first-run",
+        "--no-zygote",
+        "--disable-namespace-sandbox",
+        "--disable-seccomp-filter-sandbox",
+        "--single-process",
+        "--disable-gpu",
+        "--hide-scrollbars",
+        "--mute-audio",
+        "--disable-infobars",
+        "--disable-breakpad",
+        "--disable-web-security",
+        "--disable-extensions",
+        "--no-default-browser-check",
+        "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+      ]
+      .iter()
+      .map(OsStr::new)
+      .collect::<Vec<_>>())
     .build()
     .map_err(|e| Error::new(ErrorKind::Other, e.to_string()))
     .and_then(|options| {
